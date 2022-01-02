51c6d468f3b2cde72f32a4e2a2eb4bfa0b65090c
Shubham <shubhamsikarwar43@gmail.com>
2022-01-02 12:38:28 +0530

    Comment Delete

--- src/frontend/Components/Comment.js
+++ src/frontend/Components/Comment.js
@@ -4,21 +4,65 @@ import { AiOutlineSend } from 'react-icons/ai'
 import { useSelector } from 'react-redux'
 import './Comments.css'
 import axios from 'axios'
-import CommentCard from './CommentCard'
+import { MdDelete } from 'react-icons/md'
 
 function Comments(props) {
   const User = useSelector((state) => state.myCard)
   const cardId = props.commentData.cardId
   const [comments, setComments] = useState(props.commentData.comments)
   let [checker, setChecker] = useState(2)
+  const [db, setDB] = useState(true)
   const [moreComments, setMoreComments] = useState(false)
   const [commentText, setCommentText] = useState('')
   let index = 0
+  console.log(User._id);
+  console.log(cardId)
   const renderComment = comments
     .map((comment) => {
       if (index < checker) {
         index++
-        return <CommentCard commentData={{comment,cardId}} key={comment._id} />
+        // if (comment.name === User.name) {
+        //   setDB(true)
+        // } else {
+        //   setDB(false)
+        // }
+        // return <CommentCard commentData={{comment,cardId}} key={comment._id} /> used it for separate component
+        const deleteComment = () => {
+          const deleteData = {
+            cardId,
+            commentId: comment._id,
+          }
+          axios
+            .put('http://localhost:9000/card/deleteComment', deleteData)
+            .then((res) => {
+              setComments(res.data.comments)
+            })
+        }
+        return (
+          <div className='comment' key={comment._id}>
+            <div className='comment-card'>
+              <div className='comment-header'>
+                <img src={comment.userImage} />
+                <div>
+                  <p className='comment-name'>{comment.userName}</p>
+                  <p className='comment-date'>28/12/21</p>
+                  {db ? (
+                    <MdDelete
+                      onClick={deleteComment}
+                      className='comment-content-delete'
+                      size={25}
+                    />
+                  ) : (
+                    ''
+                  )}
+                </div>
+              </div>
+              <div className='comment-content'>
+                <p>{comment.text}</p>
+              </div>
+            </div>
+          </div>
+        )
       } else {
         return ''
       }
@@ -34,9 +78,7 @@ function Comments(props) {
     axios
       .post('http://localhost:9000/card/comment', commentData)
       .then((res) => {
-        console.log(res.data)
         if (res.data.status === 'OK') {
-          console.log(comments)
           setComments((prev) => [
             ...prev,
             { ...commentData, _id: res.data._id },
@@ -60,7 +102,8 @@ function Comments(props) {
         </div>
       </div>
       <div className='comment-numbers'>
-        <button className='comment-numbers-button'
+        <button
+          className='comment-numbers-button'
           onClick={() => {
             if (checker === comments.length) {
               setChecker(2)
--- src/frontend/Components/CommentCard.js
+++ src/frontend/Components/CommentCard.js
@@ -1,47 +1,47 @@
-import React from 'react'
-import './Comments.css'
-import { MdDelete } from 'react-icons/md'
-import axios from 'axios'
+// import React from 'react'
+// import './Comments.css'
+// import { MdDelete } from 'react-icons/md'
+// import axios from 'axios'
 
-function CommentCard({
-  commentData: {
-    comment: { userName, userImage, text, _id },
-    cardId,
-  },
-}) {
-  const deleteComment = () => {
-    const deleteData = {
-      cardId,
-      commentId: _id,
-    }
-    axios
-      .put('http://localhost:9000/card/deleteComment', deleteData)
-      .then((res) => {
-        console.log(res.data)
-      })
-  }
+// function CommentCard({
+//   commentData: {
+//     comment: { userName, userImage, text, _id },
+//     cardId,
+//   },
+// }) {
+//   const deleteComment = () => {
+//     const deleteData = {
+//       cardId,
+//       commentId: _id,
+//     }
+//     axios
+//       .put('http://localhost:9000/card/deleteComment', deleteData)
+//       .then((res) => {
+//         console.log(res.data)
+//       })
+//   }
 
-  return (
-    <div className='comment' key={_id}>
-      <div className='comment-card'>
-        <div className='comment-header'>
-          <img src={userImage} />
-          <div>
-            <p className='comment-name'>{userName}</p>
-            <p className='comment-date'>28/12/21</p>
-            <MdDelete
-              onClick={deleteComment}
-              className='comment-content-delete'
-              size={25}
-            />
-          </div>
-        </div>
-        <div className='comment-content'>
-          <p>{text}</p>
-        </div>
-      </div>
-    </div>
-  )
-}
+//   return (
+//     <div className='comment' key={_id}>
+//       <div className='comment-card'>
+//         <div className='comment-header'>
+//           <img src={userImage} />
+//           <div>
+//             <p className='comment-name'>{userName}</p>
+//             <p className='comment-date'>28/12/21</p>
+//             <MdDelete
+//               onClick={deleteComment}
+//               className='comment-content-delete'
+//               size={25}
+//             />
+//           </div>
+//         </div>
+//         <div className='comment-content'>
+//           <p>{text}</p>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
 
-export default CommentCard
+// export default CommentCard
